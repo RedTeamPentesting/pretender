3be6aebf31c2757a02904e79a5d29b4e457d6c1f
Erik Geiser <erik.geiser@redteam-pentesting.de>
2022-05-09 13:35:17 +0200

    Don't fail when no local IPv6 address is configured

--- cli.go
+++ cli.go
@@ -294,7 +294,9 @@ func configFromCLI() (config Config, logger *Logger, err error) {
 
 	config.LocalIPv6, err = getLinkLocalIPv6Address(config.Interface)
 	if err != nil && !config.NoDHCPv6DNSTakeover {
-		return config, logger, fmt.Errorf("cannot detect link local IPv6 (required for DHCPv6 DNS Takeover): %w", err)
+		logger.Errorf("cannot detect link local IPv6 (required for DHCPv6 DNS Takeover): %b", err)
+
+		config.NoDHCPv6DNSTakeover = true
 	}
 
 	config.SpoofFor, err = asHostMatchers(config.spoofFor)
